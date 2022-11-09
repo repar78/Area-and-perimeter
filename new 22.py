c8604bb49f3f71d8890811670821f05a74941f77
parastoo <parrahman79@gmail.com>
2022-11-09 17:27:51 +0330

    first commit

---
+++ new 22.py
@@ -0,0 +1,20 @@
+print(" welcome \n Please select \n")
+print(" 1.Rectangle \n 2.Square \n 3.triangle")
+number =int(input("\n number:"))
+
+if number == 1:
+    length = float(input("Enter the length of the rectangle:"))
+    width  = float(input("Enter the width of the rectangle :"))
+    print (f"masahat={length*width}")
+    print(f"mohit  ={(length+width) * 2}")   
+    
+elif number == 2:
+    side   = float(input("Write the size of the side of the square :"))
+    print(f"masahat={side*side}")
+    print(f"mohit  ={(side * 4)}")  
+    
+else:
+    base   = float(input("Enter the base of the triangle:"))
+    Height = float(input("Write the height of the triangle:"))
+    print(f"masahat={base*height}")
+
