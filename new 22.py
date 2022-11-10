e7035443507098bacadb4502094dd6cbdd6451f9
parastoo <parrahman79@gmail.com>
2022-11-10 10:23:23 +0330

    Changing the name masahat and mohit

--- new 22.py
+++ new 22.py
@@ -5,16 +5,16 @@ number =int(input("\n number:"))
 if number == 1:
     length = float(input("Enter the length of the rectangle:"))
     width  = float(input("Enter the width of the rectangle :"))
-    print (f"masahat={length*width}")
-    print(f"mohit  ={(length+width) * 2}")   
+    print (f"area={length*width}")
+    print(f"Perimeter  ={(length+width) * 2}")   
     
 elif number == 2:
     side   = float(input("Write the size of the side of the square :"))
-    print(f"masahat={side*side}")
-    print(f"mohit  ={(side * 4)}")  
+    print(f"area={side*side}")
+    print(f"Perimeter  ={(side * 4)}")  
     
 else:
     base   = float(input("Enter the base of the triangle:"))
     Height = float(input("Write the height of the triangle:"))
-    print(f"masahat={base*height}")
+    print(f"area={base*height}")
 
